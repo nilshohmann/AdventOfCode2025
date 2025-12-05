6c98a3919e5fd8be96f101f56871d1076ec311fe
Nils Hohmann <nhohmann@slashwhy.de>
2025-12-05 07:11:03 +0100

    Added solution for day 5

--- riddles/day05/day05.rb
+++ riddles/day05/day05.rb
@@ -7,7 +7,7 @@ class Day05 < Riddle
 
     private
         def validateFirst
-            return expect(calculateFirst("input_test.txt"), 1)
+            return expect(calculateFirst("input_test.txt"), 3)
         end
 
         def solveFirst
@@ -15,7 +15,7 @@ class Day05 < Riddle
         end
 
         def validateSecond
-            return expect(calculateSecond("input_test.txt"), 2)
+            return expect(calculateSecond("input_test.txt"), 14)
         end
 
         def solveSecond
@@ -23,10 +23,45 @@ class Day05 < Riddle
         end
 
         def calculateFirst(filename)
-            return 0
+            ranges, ids = readInputFile(filename).split(/\n\n/)
+            ranges = parseRanges(ranges)
+
+            result = 0
+            ids.split(/\n/).map(&:to_i).each do |id|
+                result += 1 if ranges.any? { |r| r.member?(id) }
+            end
+
+            return result
         end
 
         def calculateSecond(filename)
-            return 0
+            ranges, ids = readInputFile(filename).split(/\n\n/)
+            ranges = parseRanges(ranges).sort { |x,y| x.begin <=> y.begin }
+
+            result = 0
+            while !ranges.empty? do
+                r = ranges.shift
+
+                i = 0
+                while i < ranges.length do
+                    if ranges[i].begin <= r.end then
+                        r = r.begin .. [r.end, ranges[i].end].max
+                        ranges.delete_at(i)
+                    else
+                        i += 1
+                    end
+                end
+
+                result += r.size()
+            end
+
+            return result
+        end
+
+        def parseRanges(data)
+            data.split(/\n/).map do |line|
+                from, to = line.split(/-/)
+                from.to_i .. to.to_i
+            end
         end
 end
