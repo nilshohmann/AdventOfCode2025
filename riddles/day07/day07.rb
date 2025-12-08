7b62889f260f63dd3cc40364828a516ab370d4f3
Nils Hohmann <nhohmann@slashwhy.de>
2025-12-08 07:50:01 +0100

    Added solution for day 7

--- riddles/day07/day07.rb
+++ riddles/day07/day07.rb
@@ -7,26 +7,73 @@ class Day07 < Riddle
 
     private
         def validateFirst
-            return expect(calculateFirst("input_test.txt"), 1)
+            return expect(calculateFirst("input_test.txt"), 21)
         end
 
         def solveFirst
-            calculateFirst "input.txt"
+            calculateFirst("input.txt")
         end
 
         def validateSecond
-            return expect(calculateSecond("input_test.txt"), 2)
+            return expect(calculateSecond("input_test.txt"), 40)
         end
 
         def solveSecond
-            calculateSecond "input.txt"
+            calculateSecond("input.txt")
         end
 
         def calculateFirst(filename)
-            return 0
+            map = readInputFile(filename).split(/\n/)
+
+            beams = Set[map[0].index('S')]
+            splits = 0
+
+            (1...map.length).each do |y|
+                newBeams = Set[]
+
+                beams.each do |beam|
+                    if map[y][beam] == '^' then
+                        newBeams.add(beam-1)
+                        newBeams.add(beam+1)
+                        splits += 1
+                    else
+                        newBeams.add(beam)
+                    end
+                end
+
+                beams = newBeams
+            end
+
+            return splits
         end
 
         def calculateSecond(filename)
-            return 0
+            map = readInputFile(filename).split(/\n/)
+
+            beams = Set[map[0].index('S')]
+            counts = Array.new(map[0].length, 0)
+            counts[beams.first] = 1
+
+            (1...map.length).each do |y|
+                newBeams = Set[]
+                newCounts = Array.new(map[0].length, 0)
+
+                beams.each do |beam|
+                    if map[y][beam] == '^' then
+                        newBeams.add(beam-1)
+                        newBeams.add(beam+1)
+                        newCounts[beam-1] += counts[beam]
+                        newCounts[beam+1] += counts[beam]
+                    else
+                        newBeams.add(beam)
+                        newCounts[beam] += counts[beam]
+                    end
+                end
+
+                beams = newBeams
+                counts = newCounts
+            end
+
+            return counts.sum()
         end
 end
