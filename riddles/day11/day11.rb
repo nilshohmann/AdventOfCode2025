2c4aa4db258b722e73ac0a9540dca5d0ba977089
Nils Hohmann <nhohmann@slashwhy.de>
2025-12-11 07:53:34 +0100

    Added solution for day 11

--- riddles/day11/day11.rb
+++ riddles/day11/day11.rb
@@ -7,26 +7,67 @@ class Day11 < Riddle
 
     private
         def validateFirst
-            return expect(calculateFirst("input_test.txt"), 1)
+            return expect(calculateFirst("input_test_1.txt"), 5)
         end
 
         def solveFirst
-            calculateFirst "input.txt"
+            calculateFirst("input.txt")
         end
 
         def validateSecond
-            return expect(calculateSecond("input_test.txt"), 2)
+            return expect(calculateSecond("input_test_2.txt"), 2)
         end
 
         def solveSecond
-            calculateSecond "input.txt"
+            calculateSecond("input.txt")
         end
 
         def calculateFirst(filename)
-            return 0
+            $connections = readConnections(filename)
+            $paths = {}
+
+            return countPaths('you', 'out')
         end
 
         def calculateSecond(filename)
-            return 0
+            $connections = readConnections(filename)
+            $paths = {}
+
+            count = countPaths('svr', 'fft') * countPaths('fft', 'dac') * countPaths('dac', 'out')
+            count += countPaths('svr', 'dac') * countPaths('dac', 'fft') * countPaths('fft', 'out')
+
+            return count
+        end
+
+        def readConnections(filename)
+            connections = {}
+
+            readInputFile(filename).split(/\n/).each do |line|
+                (node, targets) = line.split(/: /)
+                connections[node] = targets.split(/ /)
+            end
+
+            return connections
+        end
+
+        def countPaths(start, target)
+            if $paths.include? [start, target] then
+                return $paths[[start, target]]
+            end
+
+            if start == target then
+                c = 1
+                $paths[[start, target]] = c
+                return c
+            end
+
+            if start == 'out' then
+                $paths[[start, target]] = 0
+                return 0
+            end
+
+            c = $connections[start].reduce(0) { |p,n| p + countPaths(n, target) }
+            $paths[[start, target]] = c
+            return c
         end
 end
